b6c428ca62429ec437e6ad9eadbeb6d170daa6b0
kkubraselcuk <kkubra.selcuk@gmail.com>
2022-08-22 16:26:54 +0300

    refresh code

--- Hafta3/Javascript-Odev2/js/style.js
+++ Hafta3/Javascript-Odev2/js/style.js
@@ -1,34 +1,70 @@
-const newInput = document.querySelector('#task');
-const buton = document.querySelector(`.button`);
-const list = document.querySelector('ul');
+let addBtn = document.getElementById("add-btn");
+let listName = document.getElementById("list-name");
+let newUl = document.querySelector("ul");
 
+let toDoLists = localStorage.getItem("toDoList")
+    ? JSON.parse(localStorage.getItem("toDoList"))
+    : [];
 
-for (let i = 0; i < list.children.length; i++) {
-    list.children[
-        i
-    ].innerHTML += `<button style="margin-right: 20px; margin-top:15px"class="close">&times</button></li>`;
+localStorage.setItem("toDoList", JSON.stringify(toDoLists));
+const data = JSON.parse(localStorage.getItem("toDoList"));
+
+function alert(a) {
+    a.classList.add("checked");
 }
-let sayac = 0;
-function newElement() {
-    if (newInput.value != '' || newInput.value != ' ') {
-        list.innerHTML += `<li>${newInput.value}<button style="margin-right: 20px; margin-top:15px" class="close">&times</button></li>`;
-        localStorage.setItem(sayac, newInput.value);
-        sayac++;
-        let toastBasarili = document.querySelector('.success');
-        let toastAlertBasarili = new bootstrap.Toast(toastBasarili);
-        toastAlertBasarili.show();
-    } else {
-        let toastBasarisiz = document.querySelector('.error');
-        let toastAlertBasarisiz = new bootstrap.Toast(toastBasarisiz);
-        toastAlertBasarisiz.show();
-    }
-}
-buton.addEventListener('submit', newElement);
 
-list.addEventListener('click', function (e) {
-    if (e.target.className == '' || e.target.className == 'checked') {
-        e.target.classList.toggle('checked');
+function alert2(a) {
+    localStorage.clear();
+    a.parentElement.remove();
+}
+
+addBtn.addEventListener("click", function () {
+    if (listName.value != 0) {
+        let newDiv = document.createElement("div");
+        newDiv.setAttribute("onclick", "alert(this);");
+
+        let newLi = document.createElement("li");
+        let newA = document.createElement("a");
+
+        newA.setAttribute("onclick", "event.stopPropagation(); alert2(this);");
+
+        newLi.innerHTML = `${listName.value}`;
+        newA.innerHTML = `&times;`;
+
+        newDiv.append(newLi, newA);
+
+        newUl.append(newDiv);
+
+        toDoLists.push(listName.value);
+        localStorage.setItem("toDoList", JSON.stringify(toDoLists));
+
+        listName.value = "";
+
+        $(document).ready(function () {
+            $(".toast.success").toast("show");
+        });
     } else {
-        e.target.parentElement.remove();
+        $(document).ready(function () {
+            $(".toast.error.hide").toast("show");
+        });
     }
+});
+function addList(text) {
+    let newDiv = document.createElement("div");
+    newDiv.setAttribute("onclick", "alert(this);");
+
+    let newLi = document.createElement("li");
+    let newA = document.createElement("a");
+
+    newA.setAttribute("onclick", "event.stopPropagation(); alert2(this);");
+
+    newLi.innerHTML = text;
+    newA.innerHTML = `&times;`;
+
+    newDiv.append(newLi, newA);
+
+    newUl.append(newDiv);
+}
+data.forEach((item) => {
+    addList(item);
 });
